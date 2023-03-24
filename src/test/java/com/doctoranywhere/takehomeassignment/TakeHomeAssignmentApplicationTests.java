2bc6ef6589eac101ef0e702375beda4e049d2fb8
Cyunyii <952374860@qq.com>
2023-03-24 16:05:01 +0800

    Initial Commit

---
+++ src/test/java/com/doctoranywhere/takehomeassignment/TakeHomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctoranywhere.takehomeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
